8eb8cfc31ec3206b54fc8e82cca91cd10c01bd7e
長尾准誠 <junsei.nagao@gmail.com>
2020-07-19 06:32:07 +0900

    make blank

--- main.js
+++ main.js
@@ -2,13 +2,7 @@
  * 「下準備」
  * HTML上でidの付いた要素を取得する
  */
-const A = document.getElementById("a");
-const B = document.getElementById("b");
-const TIMER = document.getElementById("timer");
-const RESULT_DISPLAY = document.getElementById("result-display");
-const EXPLAIN_DISPLAY = document.getElementById("explain-display");
-const CORRECT = document.getElementById("audio-correct");
-const WRONG = document.getElementById("audio-wrong");
+
 
 /**
  * 「回答発表」
@@ -18,23 +12,7 @@ const WRONG = document.getElementById("audio-wrong");
  * 「タイマーを止める」関数と「音を鳴らす」関数を呼び出します。
  * + Timeout関数を解くclearTimeout()も使います。
 */
-A.onclick = function () {
-  RESULT_DISPLAY.textContent = "正解!!!";
-  playCorrect();
 
-  //タイマー止める
-  stopTimer();
-  clearTimeout(limit);
-}
-
-B.onclick = function () {
-  RESULT_DISPLAY.textContent = "残念!!!";
-  playWrong();
-
-  //タイマー止める
-  stopTimer();
-  clearTimeout(limit);
-}
 
 /**
  * 「音を鳴らす」
@@ -44,13 +22,7 @@ B.onclick = function () {
  * 
  * 回答したときに呼び出せる様にしましょう。
  */
-const playCorrect = function () {
-  CORRECT.play();
-}
 
-const playWrong = function () {
-  WRONG.play();
-}
 
 
 /**
@@ -64,14 +36,14 @@ const playWrong = function () {
  */
 
 // 10秒分のミリ秒を変数で宣言する。(10秒=1000ms) 変数名はtimeを使う(なんでも良いけど説明のため)
-let time = 1000;
+
 
 // setIntervalでオブジェクトを入れる変数を宣言「だけ」する。 変数名はtimerを使う
 // (どこからでもアクセスできるように用意しておく)
-let timer;
+
 
 // 同じくsetTimeoutを入れる変数を用意する。 変数名はlimitを使う
-let limit
+
 
 
 /**
@@ -79,11 +51,11 @@ let limit
  */ 
 const countDown = function () {
   // timeを1減らす
-  time = time - 1;
+
 
   // timeを秒単位にしてHTMLの表示を更新する (textContentの中身を入れ替える)
   // time(マイクロ秒)を秒単位にするには time / 100
-  TIMER.textContent = time / 100;
+
 };
 
 /**
@@ -98,7 +70,7 @@ const getTime = function () {
  */
 const startTimer = function () {
   // setInterval を使って 10ms ごとに countDown を実行する モノを作る
-  timer = setInterval(countDown, 10);
+
 };
 
 /**
@@ -106,7 +78,7 @@ const startTimer = function () {
  */
 const stopTimer = function () {
   // clearInterval(setIntervalで登録した変数)と書くと登録は消える。
-  clearInterval(timer);
+
 };
 
 /**
@@ -115,14 +87,13 @@ const stopTimer = function () {
 const timeout = function () {
     
     //タイマーを止める
-    stopTimer(timer);
+
 
     // result-displayの中身を時間切れにする
-    RESULT_DISPLAY.textContent = "時間切れ!!!!";
-    stopTimer();
+
 
     // 不正解の音を再生する。
-    playWrong();
+
 }
 
 /**
@@ -130,11 +101,11 @@ const timeout = function () {
  */
 window.onload = function () {
   // タイマーをスタートさせる。
-  startTimer();
+
 
   // setTimeout(関数,時間)とするとミリ秒に止めれる。limitにいれておく。
   // 0秒以下になったら正解にしてタイマーを止める。
   // (つまり、10000ミリ秒後に不正解の時と同じことをする。)
-  limit = setTimeout(timeout, 10000);
+
 };
 
